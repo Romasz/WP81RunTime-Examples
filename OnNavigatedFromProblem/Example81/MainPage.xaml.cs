539148007e1787d9ecc6ba3a5dd3032f108d6821
Romasz <stack@romasz.net>
2014-11-21 07:23:14 +0100

    OnNavigatedFrom problem
    A sample allowing to check whether OnNaviagetdFrom is called when the
    App is Suspended.

---
+++ OnNavigatedFromProblem/Example81/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Example81
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+ 
+        private void firstBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(BasicPage1));
+        }
+
+    }
+}
