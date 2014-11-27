e005b0d56406791a32117c9897b6394e51fe117c
Romasz <stack@romasz.net>
2014-11-27 13:46:51 +0100

    Progress bar height

---
+++ ProgressBarHeight/Example81/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Example81
+{
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<string> commands = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        CommandBar previousBar;
+        private void HideBar()
+        {
+            previousBar = BottomAppBar as CommandBar;
+            BottomAppBar = null;
+        }
+
+        private void ShowBar()
+        {
+            BottomAppBar = previousBar;
+        }
+
+        private void firstBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BottomAppBar.IsOpen = true;
+        }
+
+        private async void secondBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BottomAppBar.IsOpen = false;
+        }
+    }
+}
