2a62fe1dd1a7505772c324534996de3f97ef838b
Romasz <stack@romasz.net>
2014-12-01 12:45:36 +0100

    Font sample

---
+++ FontExampleSourceSansPro/Example81/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Example81
+{
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<string> commands = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
