3fe7f1e206a41c73f98496348909a36fb7bd8ce2
Romasz <stack@romasz.net>
2014-12-15 07:14:25 +0100

    App with common appbar

---
+++ CommonAppBar/Example81/App.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace Example81
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            Frame frame = rootPage.RootFrame;
+            if (frame == null)
+            {
+                return;
+            }
+
+            if (frame.CanGoBack)
+            {
+                frame.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        PageWithAppBar rootPage;
+        public CommandBar MyAppBar { get { return rootPage.BottomAppBar as CommandBar; } }
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            rootPage = Window.Current.Content as PageWithAppBar;
+            if (rootPage == null)
+            {
+                rootPage = new PageWithAppBar();
+
+                rootPage.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+                Window.Current.Content = rootPage;
+            }
+            if (rootPage.RootFrame.Content == null)
+                rootPage.RootFrame.Navigate(typeof(MainPage), e.Arguments);
+
+            Window.Current.Activate();
+        }
+
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ CommonAppBar/Example81/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Example81
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void firstBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page1));
+        }
+
+        private void secondBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ((App.Current as App).MyAppBar.PrimaryCommands[0] as AppBarButton).Label = "New label";
+        }
+    }
+}
