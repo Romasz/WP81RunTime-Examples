7e49cde152319b7bfb59bb65f3a4dfa15d08d449
Romasz <stack@romasz.net>
2014-12-17 10:14:29 +0100

    Dimming example

---
+++ DimmingExample/Example81/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Display;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Example81
+{
+    public sealed partial class MainPage : Page
+    {
+        DisplayRequest preventDim;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (preventDim == null)
+            {
+                preventDim = new DisplayRequest();
+                preventDim.RequestActive();
+            }
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (preventDim != null)
+            {
+                preventDim.RequestRelease();
+                preventDim = null;
+            }
+        }
+    }
+}
